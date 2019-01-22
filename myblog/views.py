2652fd89c8e2dd113946ce316d950b904ab754ce
karan-chaudhari <chaudharik353@gmail.com>
2019-01-22 23:33:01 +0530

    Add json file and load the parameters

--- myblog/views.py
+++ myblog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+import json
+
+with open('config.json') as f:
+    params = json.load(f)['params']
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'params':params}
+    return render(request, 'home.html', context)
 
 def about(request):
     return render(request, 'about.html')
